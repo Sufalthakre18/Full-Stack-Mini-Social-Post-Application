fc26d80e1a3c0b45fdf1c8f315b0449170cb7c8f
Sufal Thakre <sufalthakre1@gmail.com>
2026-02-03 18:35:12 +0530

    add initial server setup with dotenv configuration and express

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import dotenv from 'dotenv'
+import express from 'express'
+import cors from 'cors'
+dotenv.config()
+
+const app= express();
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/',(_ ,res)=>{
+    res.send("api is running")
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT} `);
+})
